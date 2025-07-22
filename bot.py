7fa8affa3e9389c031f0ce1945ab5effe9b12120
DRANUJ1 <anujediter910@gmail.com>
2025-07-22 13:19:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ from utils.context_manager import ContextManager
 
 # Configure logging
 logging.basicConfig(
-    format='\%(asctime)s - %(name)s - %(levelname)s - %(message)s\',
+    format='\%(asctime)s - %(name)s - %(levelname)s - %(message)s\'
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
